79b799b7d8157f78243ad4c6dba7c9c629b1d4a5
SusiYanti17 <146178283+SusiYanti17@users.noreply.github.com>
2024-02-27 18:42:42 +0700

    Add files via upload

---
+++ week1/variabel.php
@@ -0,0 +1,11 @@
+<?php
+$nama ="Susi Yanti";
+$jurusan = "Sistem Informasi";
+$semester = "2"
+
+echo 'nama saya : ' .$nama;
+echo '</br> jurusan : ' .$jurusan;
+echo '</br> semester : ' .$semester;
+?>
+
+
